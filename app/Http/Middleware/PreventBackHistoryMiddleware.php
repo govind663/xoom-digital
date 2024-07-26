1b78613984172a879343f288a4d392882328c83d
govind663 <whitdevil06@gmail.com>
2024-07-26 13:53:58 +0530

    Xoom-Digital | Version - 0.71

--- app/Http/Middleware/PreventBackHistoryMiddleware.php
+++ app/Http/Middleware/PreventBackHistoryMiddleware.php
@@ -21,7 +21,7 @@ class PreventBackHistoryMiddleware
         $response->headers->set('X-Permitted-Cross-Domain-Policies', 'master-only');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
-        // $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+        $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
         $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate, post-check=0, pre-check=0');
         $response->headers->set('Pragma', 'no-cache');
         $response->headers->set('Expires', 'Sat, 26 Jul 1997 05:00:00 GMT');
